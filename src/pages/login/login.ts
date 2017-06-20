9daecd345a13a625b7f41b97267f68bfdc9ab30e
ricchard <rm1a14@cs.unza.zm>
2017-06-20 03:42:34 +0200

    merged into master 2

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { SignupPage } from "../pages/signup/signup";
+
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
+  sinupPage = 'SignupPage';
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  signUp(){
+    this.navCtrl.push(SignupPage);
   }
 
 }
