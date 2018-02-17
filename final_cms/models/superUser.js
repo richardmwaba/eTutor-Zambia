b4d3ad6b76473ccd8a512f7e513ac098812719de
ricchard <ricch.mwaba@gmail.com>
2018-02-17 21:20:16 +0200

    Authentication for cms complete

---
+++ final_cms/models/superUser.js
@@ -0,0 +1,72 @@
+/**
+ * Defines the super user model
+ */
+
+//Dependancies
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//Super User Schema
+const SuperUserSchema = mongoose.Schema({
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phonenumber: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    group: {
+        type: Number
+    } 
+});
+
+//Export Module
+const SuperUser = module.exports = mongoose.model('SuperUser', SuperUserSchema);
+
+//Get Super User By Id
+module.exports.getSuperUserById = function(id, callback){
+    SuperUser.findById(id, callback);
+}
+
+//Get Super User By Name
+module.exports.getSuperUserByName = function(username, callback){
+    const query = {username: username};
+    SuperUser.findOne(query, callback);
+}
+
+//Add Super User
+module.exports.addSuperUser = function(newSuperUser,callback){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newSuperUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newSuperUser.password = hash;
+            newSuperUser.save(callback);
+        });
+    });
+}
+
+//Compare Super User Password
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
---
+++ final_cms/routes/users.js
@@ -0,0 +1,76 @@
+//import { Session } from 'inspector';
+
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+
+const SuperUser = require('../models/superUser');
+
+//Register
+router.post('/register', (req, res, next) => {
+    let newSuperUser = new SuperUser({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    SuperUser.addSuperUser(newSuperUser, (err, superUser) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to register user'});
+        }
+        else
+        {
+            res.json({success: true, msg: 'User registred successfully'});
+        }
+    });
+});
+
+//Authenticate
+router.post('/authenticate', (req, res, next) => {
+   const username = req.body.username;
+   const password = req.body.password;
+
+   SuperUser.getSuperUserByName(username, (err, superUser) => {
+        if(err) throw err;
+  
+        if(!superUser){
+            return res.json({success: false, msg: 'User not found'});
+        }
+
+        SuperUser.comparePassword(password, superUser.password, (err, isMatch) => {
+            if(err) throw err;
+            if(isMatch) {
+                const token = jwt.sign(superUser.toJSON(), config.secret, {
+                    expiresIn: 604800  // 1 week(604800 sec) before token expires
+                });
+
+                // values to be returned to frontend app
+                res.json({
+                    success: true,
+                    token: "JWT "+token,
+                    superUser: {
+                        id: superUser._id,
+                        name: superUser.firstname + ' ' + superUser.lastname,
+                        username: superUser.username,
+                        email: superUser.email
+                    }
+                });
+            } else {
+                // if no match is found
+                return res.json({success: false, msg: 'Wrong password and/or username.'});
+            }
+        });
+   });
+});
+
+//Profile
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.json({superUser: req.superUser});
+});
+
+
+module.exports = router;
