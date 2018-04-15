815c907b9a2be5c6c5a1620f2d5ce8af526ff671
ricchard <ricch.mwaba@gmail.com>
2018-04-15 19:27:20 +0200

    Videos UI 2

--- final_cms/ui-src/src/app/pages/lessons/alevel/alevel.component.ts
+++ final_cms/ui-src/src/app/pages/lessons/alevel/alevel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxCarousel } from 'ngx-carousel';
 
 @Component({
   selector: 'app-alevel',
--- final_cms/ui-src/src/app/pages/lessons/nine/nine.component.ts
+++ final_cms/ui-src/src/app/pages/lessons/nine/nine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxCarousel } from 'ngx-carousel';
 
 @Component({
   selector: 'app-nine',
@@ -7,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NineComponent implements OnInit {
 
+  public carouselTileOneItems: Array<any> = [];
+  public carouselTileOne: NgxCarousel;
+  images: string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.images = [
+      'assets/images/bg.jpg',
+      'assets/images/car.png',
+      'assets/images/canberra.jpg',
+      'assets/images/holi.jpg'
+    ];
+    
+    this.carouselTileOne = {
+      grid: { xs: 2, sm: 3, md: 3, lg: 4, all: 0 },
+      speed: 600,
+      interval: 3000,
+      point: {
+        visible: false,
+        pointStyles: `
+          .ngxcarouselPoint {
+            list-style-type: none;
+            text-align: center;
+            padding: 12px;
+            margin: 0;
+            white-space: nowrap;
+            overflow: auto;
+            box-sizing: border-box;
+          }
+          .ngxcarouselPoint li {
+            display: inline-block;
+            border-radius: 50%;
+            background: #6b6b6b;
+            padding: 5px;
+            margin: 0 3px;
+            transition: .4s;
+          }
+          .ngxcarouselPoint li.active {
+              border: 2px solid rgba(0, 0, 0, 0.55);
+              transform: scale(1.2);
+              background: transparent;
+            }
+        `
+      },
+      load: 2,
+      loop: true,
+      touch: true,
+      easing: 'ease',
+      animation: 'lazy'
+    };
+
+    this.carouselTileOneLoad();
+
+  }
+
+  public carouselTileOneLoad() {
+    const len = this.carouselTileOneItems.length;
+    if (len <= 10) {
+      for (let i = len; i < len + 5; i++) {
+        this.carouselTileOneItems.push(
+          this.images[Math.floor(Math.random() * this.images.length)]
+        );
+      }
+    }
   }
 
 }
